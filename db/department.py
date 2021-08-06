fd6599fca054bf1e072107be37670fe1ce3e8a81
duwen135 <duwen135@163.com>
2021-08-06 17:07:54 +0800

    首次提交
    Signed-off-by: duwen135 <duwen135@163.com>

---
+++ db/department.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---
+# @Software: PyCharm
+# @File: department.py
+# @Author: duwen
+# @E-mail: duwen135@163.com
+# @Site: 
+# @Time: 8月 06, 2021
+# ---
